d152d317c129fe8f1a42e7fa5e48e0fcf9d72b2e
Jave <pangga.jave@gmail.com>
2021-04-02 15:17:11 +0800

    update space s cumtomer

--- customer-nav-bar-view.php
+++ customer-nav-bar-view.php
@@ -1,6 +1,5 @@
-<div>
-		<div>
-			<?php
+
+<?php
 			include('connect_db.php');
 
 			$query = "SELECT * FROM logo_tb";
@@ -11,30 +10,39 @@
 
 			$image = $rows['image'];
 			$webname = $rows['webname'];
-			?>
+
+			require_once("authin-customer.php"); ?>
+			<?php
+				include('connect_db.php');
+				
+				$email_owner=$_SESSION['session_email'];
 	
-			<h2>
-<img  src="data:image/jpeg;base64,<?php echo $image;?>" style="margin-top:20px;height:20px;width:20px;"/>
-		
-			<?php echo $webname;?>
-	</h2>
-</div>
-	<div>
-<?php require_once("authin-customer.php"); ?>
-		<?php
-			include('connect_db.php');
-			
-			$email_owner=$_SESSION['session_email'];
+	$query = "SELECT * FROM product_tb WHERE cmail='$email_owner'";
+				$result = mysqli_query($con,$query);
 
-$query = "SELECT * FROM product_tb WHERE cmail='$email_owner'";
-			$result = mysqli_query($con,$query);
-?>
-				<button ><a href="customer-home-view.php">Home</a></button>				
-				<button ><a href='customer-cart-list-view.php?email_owner=<?php echo $email_owner;?>'>View Cart List</a></button>
-				<button ><a href="customer-logout-process.php">Logout</a></button>
-			
-			<h2>
-		</div>
+			?>
 
-
-</div>
+ <!-- Static navbar -->
+ <nav class="navbar navbar-default" style="background-color: #b2b2b2;">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+			<img  src="data:image/jpeg;base64,<?php echo base64_encode($image);?>" style="height:50px;width:50px;"/>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li><a href="customer-home-view.php"><?php echo$webname?></a></li>
+              <li><a href="customer-home-view.php">Home</a></li>    
+              <li><a href="customer-cart-list-view.php?email_owner=<?php echo $email_owner;?>">View Cart List</a></li>    
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="customer-logout-process.php">Logout</a></li>  
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
