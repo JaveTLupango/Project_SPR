84da17bca4a8afa457acfcd2c6417e6ab32b00f6
Jave <pangga.jave@gmail.com>
2021-04-02 14:05:38 +0800

    update

--- admin-logodelete-view.php
+++ admin-logodelete-view.php
@@ -1,14 +1,63 @@
 <?php require_once("authin.php"); ?>
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
 <head>
-	<?php include('logo-title.php');?>
+	<?php //include('logo-title.php');?>
 	<title></title>
 </head>
 <body>
 	<div>
-	<?php include('admin-nav-bar-view.php'); ?>
+	<?php //include('admin-nav-bar-view.php'); ?>
 </div>
+</table>
+</div>
+</body>
+</html> -->
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="canonical" href="https://getbootstrap.com/docs/3.4/examples/navbar/">
+
+    <title>Home</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://getbootstrap.com/docs/3.4/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="https://getbootstrap.com/docs/3.4/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="https://getbootstrap.com/docs/3.4/examples/navbar/navbar.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="https://getbootstrap.com/docs/3.4/assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+
+     <?php include 'admin-nav-bar-view.php'; ?>
+
+      <!-- Main component for a primary marketing message or call to action -->
+      <div class="jumbotron">
+			
 <?php
 			include('connect_db.php');
 
@@ -35,7 +84,7 @@
 ?>
 
 <tr>
-	<th><strong><img  src="<?php echo $image;?>" style="margin-top:20px;height:100px;width:100px;"/></strong></th>
+	<th><strong><img  src="data:image/jpeg;base64,<?php echo base64_encode($image);?>" style="margin-top:20px;height:100px;width:100px;"/></strong></th>
 	
 	<th><strong><?php echo ($webname); ?></strong></th>
 	<th><strong><?php echo ($webdesc); ?></strong></th>
@@ -44,7 +93,18 @@
 	<th colspan="4"><a style='margin-left:20px;' href='admin-logodelete-process.php?logo_id=<?php echo $logo_id;?>'><button>Delete Product</button></a></th>
 </tr>
 <?php } ?>
-</table>
-</div>
-</body>
+      </div>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="https://getbootstrap.com/docs/3.4/assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="https://getbootstrap.com/docs/3.4/dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="https://getbootstrap.com/docs/3.4/assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
 </html>
+
--- admin-orderlist-view.php
+++ admin-orderlist-view.php
@@ -1,13 +1,13 @@
 <?php require_once("authin.php"); ?>
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
 <head>
-	<?php include('logo-title.php');?>
+	<?php //include('logo-title.php');?>
 	<title>webtitle-pagetitle</title>
 </head>
 <body>
 <div>
-	<?php include('admin-nav-bar-view.php'); ?>
+	<?php //include('admin-nav-bar-view.php'); ?>
 </div>
 
 	<div>
@@ -15,10 +15,112 @@
 			<h2>Product list</h2>
 		</div>
 			<?php
+			// include('connect_db.php');
+			// $query = "SELECT * FROM order_tb";
+			// $result = mysqli_query($con,$query);		
+			?>
+
+<div align="center">
+<table width="80%">
+<tr>
+	<td>Cart No.</td>
+	<td>ProductNo</td>
+	<td>ProductName</td>
+	<td>Product Description</td>
+	<td>Quantity</td>
+	<td>Price</td>
+	<td>total ammount</td>
+
+
+</tr>
+
+<?php
+		
+	// while ($rows = mysqli_fetch_assoc($result)) 
+	// 	{
+			
+	// 		$order_no = $rows['order_no'];
+	// 		$product_id = $rows['product_no'];
+	// 		$productname = $rows['productname'];
+	// 		$productdesc = $rows['productdesc'];
+	// 		$price = $rows['price'];
+	// 		$itemno = $rows['itemno'];
+	// 		$email=$rows['email'];
+	// 		$firstname=$rows['firstname'];
+	// 		$lastname=$rows['lastname'];
+	// 		$address=$rows['address'];
+?>
+
+	<tr>
+	
+	<th><strong><?php //echo ($order_no); ?></strong></th>
+	<th><strong><?php //echo ($product_id); ?></strong></th>
+	<th><strong><?php //echo ($productname); ?></strong></th>
+	<th><strong><?php //echo ($productdesc); ?></strong></th>
+	<th ><strong><?php ///echo ($itemno); ?></strong></th>
+	<th><strong><?php //echo '₱'.$price.'.00'; ?></strong></th>
+	<th><strong><?php //echo '₱'.$price*$itemno.'.00'; ?></strong></th>
+<th>
+	<a style='margin-left:20px;' href='admin-delete-orderlist-process.php?order_no=<?php //echo $order_no;?>'><button>Customer Recieved Orsder</button></a></th>
+	</tr>
+</table>
+
+</div>
+</div>
+</body>
+</html>			 -->
+
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="canonical" href="https://getbootstrap.com/docs/3.4/examples/navbar/">
+
+    <title>Home</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://getbootstrap.com/docs/3.4/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="https://getbootstrap.com/docs/3.4/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="https://getbootstrap.com/docs/3.4/examples/navbar/navbar.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="https://getbootstrap.com/docs/3.4/assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+
+     <?php include 'admin-nav-bar-view.php'; ?>
+
+      <!-- Main component for a primary marketing message or call to action -->
+      <div class="jumbotron">
+	  <div>
+		<div>
+			<h2>Product list</h2>
+		</div>
+			<?php
 			include('connect_db.php');
-
 			$query = "SELECT * FROM order_tb";
-
 			$result = mysqli_query($con,$query);		
 			?>
 
@@ -70,5 +172,17 @@
 
 </div>
 </div>
-</body>
-</html>			
+      </div>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="https://getbootstrap.com/docs/3.4/assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="https://getbootstrap.com/docs/3.4/dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="https://getbootstrap.com/docs/3.4/assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
